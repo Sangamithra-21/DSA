57eb2c89f426fa4d867a9939fd09151955b706b4
Sangamithra <sangamithra671@gmail.com>
2023-10-07 15:21:23 +0530

    Simplifypath

---
+++ Simplifypath.java
@@ -0,0 +1,44 @@
+//-------------------------------------------------------SIMPLIFY PATH---------------------------------------------------------------
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> S = new Stack<>();
+        String result = "";
+
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == '/') {
+                continue;
+            }
+
+            StringBuilder temp = new StringBuilder();
+            while (i < path.length() && path.charAt(i) != '/') {
+                temp.append(path.charAt(i));
+                i++;
+            }
+
+            if (temp.toString().equals(".")) {
+                continue;
+            } 
+            else if (temp.toString().equals("..")) 
+            {
+                if (!S.isEmpty()) {
+                    S.pop();
+                }
+            } 
+            else {
+                S.push(temp.toString());
+            }
+        }
+
+        while (!S.isEmpty()) {
+            result = "/" + S.pop() + result;
+        }
+
+        if (result.length() == 0) {
+            return "/";
+        }
+
+        return result;
+    }
+}
